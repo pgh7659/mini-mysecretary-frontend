b812af45eb540c7b1d6952669c0d83f2e7316931
ghpark <ghpark@openit.co.kr>
2021-03-08 17:12:40 +0900

    axios Test

---
+++ src/lib/axios/http.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// Interceptor 설정 !!
+// const interceptor = axios.interceptors;
+// interceptor.request.use()
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete
+}
---
+++ src/lib/axios/service/testService.js
@@ -0,0 +1,12 @@
+import http from '../http';
+
+const testService = {
+  getList: () => {
+    const today = new Date().toLocaleDateString('fr-ca');
+    const register = 'ghpark';
+  
+    return http.get(`/api/todos?date=${today}&register=${register}`);
+  }
+}
+
+export default testService;
